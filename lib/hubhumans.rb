f12ab080f134eb2a8bca7dfaa04d81c785ead26a
Matthew Rothenberg <mrothenberg@gmail.com>
2013-02-17 23:11:36 -0500

    move main logic into lib

--- lib/hubhumans.rb
+++ lib/hubhumans.rb
@@ -1,5 +1,20 @@
 require "hubhumans/version"
+require "octokit"
 
 module Hubhumans
-  # Your code goes here...
+  
+  def Hubhumans.humanify(org_name)
+    client = Octokit::Client.new(:auto_traversal => true)
+    gh_org = client.organization_members(org_name)
+    puts "/* TEAM */"
+    puts
+    gh_org.each do |member|
+      user = client.user member.login
+      puts "  #{user.name} (#{user.login})"
+      puts "  Site: #{user.blog}" unless user.blog.nil?
+      puts "  Location: #{user.location}" unless user.location.nil?
+      puts
+    end
+  end
+
 end
